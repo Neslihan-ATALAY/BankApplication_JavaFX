597c3360c03732fff213f8fcf55cb7b68f5178a8
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-03-30 22:13:54 +0300

    Create IGenericsMethod.java

---
+++ src/main/java/com/neslihanatalay/BankApp/dao/IGenericsMethod.java
@@ -0,0 +1,13 @@
+package com.neslihanatalay.BankApp.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public interface IGenericsMethod<T> {
+
+    public T mapToObjectDTO(ResultSet resultSet) throws SQLException;
+
+    public Optional<T> selectSingle(String sql, Object... params);
+
+}
