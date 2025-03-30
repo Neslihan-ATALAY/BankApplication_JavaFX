dcd6f91bfc63e506566a7aabda1451041937425e
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-03-30 21:25:27 +0300

    Create FXMLPath.java

---
+++ src/main/java/com/neslihanatalay/BankApp/utils/FXMLPath.java
@@ -0,0 +1,14 @@
+package com.neslihanatalay.BankApp.utils;
+
+public class FXMLPath {
+
+    public static final String INDEX = "/com/neslihanatalay/BankApp/views/index.fxml";
+    public static final String LOGIN = "/com/neslihanatalay/BankApp/views/login.fxml";
+    public static final String USERREGISTER = "/com/neslihanatalay/BankApp/views/userRegister.fxml";
+    public static final String ACCOUNTREGISTER = "/com/neslihanatalay/BankApp/views/accountRegister.fxml";
+    public static final String ACCOUNTUPDATE = "/com/neslihanatalay/BankApp/views/accountUpdate.fxml";
+    public static final String ACCOUNTGETMONEY = "/com/neslihanatalay/BankApp/views/accountGetMoney.fxml";
+    public static final String ACCOUNTSETMONEY = "/com/neslihanatalay/BankApp/views/accountSetMoney.fxml";
+    public static final String SENDMONEY = "/com/neslihanatalay/BankApp/views/transactionRegister.fxml";
+
+}
